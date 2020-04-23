53691b141b1c1db74c026cf6d905c3e98c950054
louckousse <louckousse@flatbrains.me>
2020-04-24 01:39:31 +0200

    Add tap dance for shift

--- keyboards/xd68/keymaps/louckousse/keymap.c
+++ keyboards/xd68/keymaps/louckousse/keymap.c
@@ -4,31 +4,10 @@
 #define _FL 1
 #define _GL 2
 
-typedef struct {
-  bool is_press_action;
-  int state;
-} tap;
-
-enum {
-  SINGLE_TAP = 1,
-  SINGLE_HOLD = 2,
-  DOUBLE_TAP = 3,
-  DOUBLE_HOLD = 4,
-  DOUBLE_SINGLE_TAP = 5, //send two single taps
-  TRIPLE_TAP = 6,
-  TRIPLE_HOLD = 7
-};
-
 //Tap dance enums
 enum {
   X_SFT = 0,
 };
-
-int cur_dance (qk_tap_dance_state_t *state);
-
-//for the x tap dance. Put it here so it can be used in any keymap
-void x_finished (qk_tap_dance_state_t *state, void *user_data);
-void x_reset (qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap _BL: (Base Layer) Default Layer
@@ -68,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FL] = LAYOUT_65_ansi(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS,
         _______, KC_MPLY, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_INS, _______, KC_PSCR, _______, _______, _______, RESET,
-        _______, KC_MPRV, KC_VOLD, KC_MNXT, _______, TO(_GL), _______, _______, _______, LCA(KC_L), _______, _______, _______, DEBUG,
+        _______, KC_MPRV, KC_VOLD, KC_MNXT, _______, TO(_GL), _______, _______, KC_SYSTEM_SLEEP, LCA(KC_L), _______, _______, _______, DEBUG,
         _______, _______, _______, _______, _______, _______, _______, KC_PAUS, _______, _______, _______, _______, KC_PGUP, _______,
         _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END),
 
@@ -145,55 +124,28 @@ void matrix_scan_user(void) {
     }
 }
 
-int cur_dance (qk_tap_dance_state_t *state) {
-  if (state->count == 1) {
-    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
-    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
-    else return SINGLE_HOLD;
-  }
-  else if (state->count == 2) {
-    /*
-     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-     * keystrokes of the key, and not the 'double tap' action/macro.
-    */
-    if (state->interrupted) return DOUBLE_SINGLE_TAP;
-    else if (state->pressed) return DOUBLE_HOLD;
-    else return DOUBLE_TAP;
-  }
-  //Assumes no one is trying to type the same letter three times (at least not quickly).
-  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
-  if (state->count == 3) {
-    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
-    else return TRIPLE_HOLD;
-  }
-  else return 8; //magic number. At some point this method will expand to work for more presses
+void tap_key(uint16_t keycode)
+{
+  register_code  (keycode);
+  unregister_code(keycode);
 }
 
-//instanalize an instance of 'tap' for the 'x' tap dance.
-static tap xtap_state = {
-  .is_press_action = true,
-  .state = 0
-};
-
-void x_finished (qk_tap_dance_state_t *state, void *user_data) {
-  xtap_state.state = cur_dance(state);
-  switch (xtap_state.state) {
-    case SINGLE_TAP: set_oneshot_mods(MOD_LSFT); break;
-    case SINGLE_HOLD: register_code(KC_LSFT);   break;           // on hold down
-    case DOUBLE_TAP: register_code(KC_CAPS); break;
+void caps(qk_tap_dance_state_t *state, void *user_data)
+{
+  if (state->count > 1) {
+    tap_key(KC_CAPS);
+  }
+  else {
+    set_oneshot_mods(MOD_LSFT);
+    register_code   (KC_LSFT);              // on hold down
   }
 }
 
-void x_reset (qk_tap_dance_state_t *state, void *user_data) {
-  switch (xtap_state.state) {
-    case SINGLE_HOLD: unregister_code(KC_LSFT); break;
-    case DOUBLE_TAP: unregister_code(KC_CAPS); break;
-  }
-  xtap_state.state = 0;
+void caps_reset(qk_tap_dance_state_t *state, void *user_data)
+{
+  unregister_code(KC_LSFT);
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [X_SFT]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,x_finished, x_reset)
+  [X_SFT]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,caps, caps_reset)
 };
