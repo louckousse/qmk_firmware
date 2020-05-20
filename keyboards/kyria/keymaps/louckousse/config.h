6c6b0fb4b90efcf632066b2606688fff58e8b1ec
louckousse <louckousse@flatbrains.me>
2020-05-21 01:36:38 +0200

    Almost final layout with colemak and less layer toggle

--- keyboards/kyria/keymaps/louckousse/config.h
+++ keyboards/kyria/keymaps/louckousse/config.h
@@ -21,17 +21,18 @@
 #endif
 
 #ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_EFFECT_CHRISTMAS
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_SLEEP
+  #define RGBLIGHT_LAYERS
 #endif
 
 // EC11K encoders have a different resolution than other EC11 encoders.
 // When using the default resolution of 4, if you notice your encoder skipping
 // every other tick, lower the resolution to 2.
-#define ENCODER_RESOLUTION 1
+#define ENCODER_RESOLUTION 2
 
 // The Leader key allows to flexibly assign macros to key sequences.
 #define LEADER_PER_KEY_TIMING
