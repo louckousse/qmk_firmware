2d017ae5385044105291d68e9217023fe81200e3
Lucas D <6485933+louckousse@users.noreply.github.com>
2020-11-28 11:49:24 +0100

    User folder (#7)
    * My personnal xd68 mapping (#1)
    
    * Add my first mapping
    
    * First mapping, need evol
    
    * Update compose and leader keys
    
    * Add some leader key actions
    
    * Add tap dance for shift
    
    * Add tap dance for shift
    
    * Tweak the leader key timing
    
    * revert in part the left shift comportment
    
    * Add my personal Kyria keymaps (#3)
    
    * add Zima keymap (#4)
    
    * add Zima to my repository with my personal keymap
    
    * correct oled display according to rgb mode
    
    * add user space
    
    * adapt user space to the kyria layout
    
    * :shower: move the files I don't use
    
    * move the custom keycodes to userspace
    
    * Bastyl (#6)
    
    * move the custom keycodes to userspace
    
    * bastyl keymap
    
    * change the layout to respect the placement of thumb cluster
    
    * move mins and esc
    
    * Zima doesn't need user folder
    
    * vscode should not be shared

--- keyboards/kyria/keymaps/louckousse/keymap.c
+++ keyboards/kyria/keymaps/louckousse/keymap.c
@@ -14,6 +14,61 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+<<<<<<< HEAD
+#include "louckousse.h"
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [CMT] = LAYOUT_kyria_pretty_wrapper(
+      KC_TAB,  ______________COLEMAK_DH_MT_L1_____________,                                     ______________COLEMAK_DH_MT_R1_____________,  KC_CIRC,
+      KC_ESC,  ______________COLEMAK_DH_MT_L2_____________,                                     ______________COLEMAK_DH_MT_R2_____________,  KC_QUOT,
+      KC_CAPS, ______________COLEMAK_DH_MT_L3_____________, KC_RALT, KC_PAUS, PRINT,   KC_OS,   ______________COLEMAK_DH_MT_R3_____________,  KC_GRV,
+                                 XXXXXXX, XXXXXXX, KC_MINS, FUN_ENT, NAV_DEL, SYM_BSP, NUM_SPC, KC_TAB,  XXXXXXX, XXXXXXX
+    ),
+
+    [FUNMT] = LAYOUT_kyria_pretty_wrapper(
+      XXXXXXX, __________________FUN_L1___________________,                                     ___________________BLANK___________________, XXXXXXX,
+      XXXXXXX, __________________FUN_L2___________________,                                     ___________________MOD_R___________________, XXXXXXX,
+      XXXXXXX, __________________FUN_L3___________________, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, KC_ALGR, _______, XXXXXXX,
+                                 XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+    ),
+
+    [NAVMT] = LAYOUT_kyria_pretty_wrapper(
+      XXXXXXX, __________________NAV_L1___________________,                                     KC_CIRC, GO_GAME, KC_OS,   GO_SC2,  _______, XXXXXXX,
+      XXXXXXX, __________________NAV_L2___________________,                                     KC_QUOT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
+      XXXXXXX, __________________NAV_L3___________________, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_GRV,  _______, _______, KC_ALGR, _______, XXXXXXX,
+                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC, KC_SPC,  KC_ENT, XXXXXXX, XXXXXXX
+    ),
+
+    [SYMMT] = LAYOUT_kyria_pretty_wrapper(
+      XXXXXXX, __________________SYM_L1___________________,                                     ________________NUMBER_L1__________________, XXXXXXX,
+      XXXXXXX, __________________SYM_L2___________________,                                     ________________NUMBER_L2__________________, XXXXXXX,
+      XXXXXXX, __________________SYM_L3___________________, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ________________NUMBER_L3__________________, XXXXXXX,
+                                 XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+    ),
+
+    [NUMMT] = LAYOUT_kyria_pretty_wrapper(
+      XXXXXXX, ___________________BLANK___________________,                                     ________________NUMBER_L1__________________, XXXXXXX,
+      XXXXXXX, ___________________MOD_L___________________,                                     ________________NUMBER_L2__________________, XXXXXXX,
+      XXXXXXX, _______, KC_ALGR, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ________________NUMBER_L3__________________,  XXXXXXX,
+                                 XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+    ),
+
+    [LWR] = LAYOUT_kyria_pretty_wrapper(
+      KC_TL,   __________________SYM_L1___________________,                                     ________________NUMBER_L1__________________, KC_COMM,
+      KC_BT,   __________________SYM_L2___________________,                                     ________________NUMBER_L2__________________, KC_DOT,
+      KC_QT,   __________________SYM_L3___________________, _______, _______, GO_GAME, _______, ________________NUMBER_L3__________________,  _______,
+                                 ___________________BLANK___________________, ___________________BLANK___________________
+    ),
+
+    [RSE] = LAYOUT_kyria_pretty_wrapper(
+      PRINT, KC_HOME,C(KC_LEFT), KC_UP,C(KC_RGHT), KC_PGUP,                                     KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______,
+      LOCK,    KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                                     KC_F5,   KC_F6,   KC_F7,   KC_F8,  _______, _______,
+      KC_SLEP, _______, KC_MPRV, KC_MNXT, DSK_PRV, DSK_NXT, _______, GO_SC2,  _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,  _______, _______,
+                                 ___________________BLANK___________________, ___________________BLANK___________________
+=======
 #include "other.h"
 
 // #define KC_MED TD(0)
@@ -106,6 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       LOCK,    KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                                     KC_F5,   KC_F6,   KC_F7,   KC_F8,  _______, _______,
       KC_SLEP, _______, KC_MPRV, KC_MNXT, DSK_PRV, DSK_NXT, _______, GO_SC2,  _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,  _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+>>>>>>> main
     ),
 
     [SC2_B] = LAYOUT(
@@ -136,6 +192,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
+<<<<<<< HEAD
+=======
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -216,3 +274,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+>>>>>>> main
---
+++ users/louckousse/louckousse.h
@@ -0,0 +1,74 @@
+#pragma once
+#include QMK_KEYBOARD_H
+
+#include <stdio.h>
+#include "wrappers.h"
+
+/* Define layer names */
+enum userspace_layers {
+    CMT = 0,
+    NAVMT,
+    FUNMT,
+    NUMMT,
+    SYMMT,
+    LWR,
+    RSE,
+    SC2_B,
+    SC2_L,
+    GAME_B,
+    GAME_L
+};
+
+typedef union {
+    uint32_t raw;
+    struct {
+        bool osIsLinux;
+    };
+} user_config_t;
+
+user_config_t user_config;
+
+enum custom_keycodes {
+    KC_GG = SAFE_RANGE,
+    KC_GL,
+    DSK_NXT,
+    DSK_PRV,
+    PRINT,
+    LOCK,
+    KC_OS,
+    KC_BT,
+    KC_QT,
+    KC_TL,
+};
+
+#define KC_MED KC_MPLY
+#define ST_ENT SFT_T(KC_ENT)
+#define CTRL_MN CTL_T(KC_MINS)
+#define ALT_BO ALT_T(KC_LBRC)
+#define ALT_BE ALT_T(KC_RBRC)
+#define RSE_DEL LT(RSE, KC_DEL)
+#define LWR_BSP LT(LWR, KC_BSPC)
+#define ST_SPC SFT_T(KC_SPC)
+#define GO_SC2 DF(SC2_B)
+#define GO_GAME DF(GAME_B)
+#define KC_EUR ALGR(KC_5)
+// Mod tap specific keycodes
+#define NAV_DEL LT(NAVMT, KC_DEL)
+#define FUN_ENT LT(FUNMT,KC_ENT)
+#define NUM_SPC LT(NUMMT,KC_SPC)
+#define SYM_BSP LT(SYMMT,KC_BSPC)
+#define MK_A LGUI_T(KC_A)
+#define MK_R LALT_T(KC_R)
+#define MK_S LCTL_T(KC_S)
+#define MK_T LSFT_T(KC_T)
+#define MK_N LSFT_T(KC_N)
+#define MK_E LCTL_T(KC_E)
+#define MK_I LALT_T(KC_I)
+#define MK_O LGUI_T(KC_O)
+#define MK_X ALGR_T(KC_X)
+#define MK_DOT ALGR_T(KC_DOT)
+
+#define LAYOUT_kyria_pretty_wrapper(...) LAYOUT(__VA_ARGS__)
+#define LAYOUT_bastyl_pretty_wrapper(...) LAYOUT_5x6(__VA_ARGS__)
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record);
