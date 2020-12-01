b2f0039d43bc2292a2ed0966907443b2d8050630
louckousse <louckousse@flatbrains.me>
2020-12-01 22:00:11 +0100

    remove old zima folder

--- keyboards/zima/keymaps/louckousse/rules.mk
+++
@@ -1,3 +0,0 @@
-EXTRAFLAGS += -Wno-error=unused-variable -Wno-error=unused-function
-EXTRAFLAGS += -Wno-unused-function -Wno-unused-variable
-LEADER_ENABLE = no        # Enable the Leader Key feature
